baf71a9b93ff4b758d6480b47d23d14364b28061
dhinakaran011 <dhinakaran30122000@gmail.com>
2023-02-07 18:03:23 +0530

    i am going to upload

---
+++ India.java
@@ -0,0 +1,14 @@
+package india;
+
+abstract class India {
+	abstract void speakLanguage();
+	abstract void eat();
+	abstract void dress();
+	static String capital = "newDelhi";
+	public India(String primeMinister)
+	{
+	System.out.println("our Prime Minister is" + primeMinister);
+	}
+	
+
+}
